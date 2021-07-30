5ba24542e04e90de54e31223a43e201293ade9da
HyojinKim <gywls00100@gmail.com>
2021-07-31 01:48:33 +0900

    백준 2231번

---
+++ kimhyojin/boj/2231.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+answer = 1000000
+
+for i in range(1, n+1):
+    tmp = sum(map(int, str(i)))
+    tmp += i
+
+    if tmp == n:
+        answer = min(i, answer)
+        print(answer)
+        break
+    if i == n:
+        print(0)
